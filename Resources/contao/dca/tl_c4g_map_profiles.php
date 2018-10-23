66fca902f356c60d687c8dd0f7a5bfe1ce18b657
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2018-10-23 17:01:37 +0200

    added detour-handling to route

--- Resources/contao/dca/tl_c4g_map_profiles.php
+++ Resources/contao/dca/tl_c4g_map_profiles.php
@@ -5,16 +5,50 @@
  * Date: 22.10.18
  * Time: 11:19
  */
-$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['subpalettes']['router'] = 'router_api_selection,router_viaroute_url,router_attribution,router_alternative,router_from_locstyle,router_to_locstyle,router_point_locstyle,router_interim_locstyle,routerLayers';
-$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['routerLayers'] = array(
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['subpalettes']['router'] = 'router_api_selection,router_viaroute_url,router_attribution,router_alternative,router_from_locstyle,router_to_locstyle,router_point_locstyle,router_interim_locstyle,routerLayers,minDetourArea,maxDetourArea,minDetourRoute,maxDetourRoute';
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['routerLayers'] = [
     'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['routerLayers'],
     'exclude'                 => true,
     'inputType'               => 'multiColumnWizard',
-    'eval'                    => array(
-        'columnsCallback'        => array('tl_c4g_map_profiles_router','getRouterLayer')
-    ),
+    'eval'                    => [
+        'columnsCallback'        => ['tl_c4g_map_profiles_router','getRouterLayer']
+    ],
     'sql'                     => 'blob NULL'
-);
+];
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['minDetourArea'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['minDetourArea'],
+    'filter'                  => false,
+    'inputType'               => 'text',
+    'default'                 => '0',
+    'eval'                    => array( 'tl_class'=>'w50', 'rgxp'=>'digit'),
+    'sql'                     => "decimal(10) NOT NULL default '0'"
+];
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['maxDetourArea'] = [
+
+    'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['maxDetourArea'],
+    'filter'                  => false,
+    'inputType'               => 'text',
+    'default'                 => '20',
+    'eval'                    => array( 'tl_class'=>'w50', 'rgxp'=>'digit'),
+    'sql'                     => "decimal(10) NOT NULL default '20'"
+];
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['minDetourRoute'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['minDetourRoute'],
+    'filter'                  => false,
+    'inputType'               => 'text',
+    'default'                 => '0',
+    'eval'                    => array( 'tl_class'=>'w50', 'rgxp'=>'digit'),
+    'sql'                     => "decimal(10) NOT NULL default '0'"
+];
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['maxDetourRoute'] = [
+
+    'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['maxDetourRoute'],
+    'filter'                  => false,
+    'inputType'               => 'text',
+    'default'                 => '5',
+    'eval'                    => array( 'tl_class'=>'w50', 'rgxp'=>'digit'),
+    'sql'                     => "decimal(10) NOT NULL default '5'"
+];
 class tl_c4g_map_profiles_router extends Backend
 {
     public function getRouterLayer($multiColumnWizard)
@@ -46,7 +80,7 @@ class tl_c4g_map_profiles_router extends Backend
             'label'     => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['routerLayer']['label'],
             'filter'                  => false,
             'inputType'               => 'text',
-            'eval'                    => ['mandatory'=>'true', 'tl_class'=>'w50']
+            'eval'                    => ['tl_class'=>'w50']
         ];
         $return = [
             'layers'    => $arrColumnLayers,
