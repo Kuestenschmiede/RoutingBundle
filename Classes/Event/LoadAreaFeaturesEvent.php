0f61e7acec32089a3ad38c37ddf30fc37f9a6b98
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2018-11-26 16:56:48 +0100

    add attributions

--- Classes/Event/LoadAreaFeaturesEvent.php
+++ Classes/Event/LoadAreaFeaturesEvent.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: cro
- * Date: 18.10.18
- * Time: 15:49
+ * con4gis - the gis-kit
+ *
+ * @version   php 7
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
+ * @link      https://www.kuestenschmiede.de
  */
 
 namespace con4gis\RoutingBundle\Classes\Event;
