917c5b8cec88b1353f6af8a879070908c701b87a
coastforge-cro <claudio.ross@kuestenschmiede.de>
2018-12-03 17:45:28 +0100

    Remove interaction adding

--- Resources/public/js/c4g-routing.js
+++ Resources/public/js/c4g-routing.js
@@ -512,12 +512,6 @@ export class Router extends Sideboard {
     };
 
     this.options.mapController.map.on('click', self.fnMapRouterInteraction);
-    if (self.options.mapController.map.getInteractions().getArray()['9']) {
-      self.options.mapController.map.getInteractions().getArray()['9'].setActive(true);
-    }
-    else {
-      self.options.mapController.map.getInteractions().getArray()['8'].setActive(true)
-    }
   }
 
   /**
