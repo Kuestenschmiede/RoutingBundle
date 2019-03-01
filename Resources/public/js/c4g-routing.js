e6f02fa355f1beb0402e9553ff288e82c3d51e0f
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-03-01 15:11:44 +0100

    Cleanup

--- Resources/public/js/c4g-routing.js
+++ Resources/public/js/c4g-routing.js
@@ -3364,21 +3364,6 @@ export class Router extends Sideboard {
           }
         }
       }
-      // var msg = new SpeechSynthesisUtterance();
-      // var voices = window.speechSynthesis.getVoices();
-      // msg.voice = voices[10]; // Note: some voices don't support altering params
-      // msg.voiceURI = 'native';
-      // msg.volume = 1; // 0 to 1
-      // msg.rate = 1; // 0.1 to 10
-      // msg.pitch = 1.5; //0 to 2
-      // msg.text = element.html();
-      // msg.lang = 'en-US';
-      //
-      // msg.onend = function(e) {
-      //   console.log('Finished in ' + event.elapsedTime + ' seconds.');
-      // };
-      //
-      // speechSynthesis.speak(msg);
 
     };
 
