a8ffac45eb2e00a4c756b6aefb599eeac9b15555
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2019-03-06 16:31:36 +0100

    add / replace hints

--- Resources/contao/dca/tl_c4g_maps.php
+++ Resources/contao/dca/tl_c4g_maps.php
@@ -1,15 +1,15 @@
 <?php
 /*
-  * This file is part of con4gis,
-  * the gis-kit for Contao CMS.
-  *
-  * @package   	con4gis
-  * @version    6
-  * @author  	  con4gis contributors (see "authors.txt")
-  * @license 	  LGPL-3.0-or-later
-  * @copyright 	Küstenschmiede GmbH Software & Design
-  * @link       https://www.con4gis.org
-  */
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package    con4gis
+ * @version    6
+ * @author     con4gis contributors (see "authors.txt")
+ * @license    LGPL-3.0-or-later
+ * @copyright  Küstenschmiede GmbH Software & Design
+ * @link       https://www.con4gis.org
+ */
 foreach ($GLOBALS['TL_DCA']['tl_c4g_maps']['palettes'] as $id => $pallete) {
     $newPallete = str_replace('popup_info,', 'popup_info,routing_to,', $pallete);
     $GLOBALS['TL_DCA']['tl_c4g_maps']['palettes'][$id] = $newPallete;
--- Resources/contao/languages/en/tl_c4g_map_profiles.php
+++ Resources/contao/languages/en/tl_c4g_map_profiles.php
@@ -1,15 +1,15 @@
 <?php
 /*
-  * This file is part of con4gis,
-  * the gis-kit for Contao CMS.
-  *
-  * @package   	con4gis
-  * @version    6
-  * @author  	  con4gis contributors (see "authors.txt")
-  * @license 	  LGPL-3.0-or-later
-  * @copyright 	Küstenschmiede GmbH Software & Design
-  * @link       https://www.con4gis.org
-  */
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package    con4gis
+ * @version    6
+ * @author     con4gis contributors (see "authors.txt")
+ * @license    LGPL-3.0-or-later
+ * @copyright  Küstenschmiede GmbH Software & Design
+ * @link       https://www.con4gis.org
+ */
 
 /** Routing und Umkreissuche  */
 $GLOBALS['TL_LANG']['tl_c4g_map_profiles']['router'] = ['Activate routing','Please activate search tools in addition. Adds an additional button to activate routing functionality. Please check the <a href="https://github.com/DennisOSRM/Project-OSRM/wiki/Api-usage-policy" target="_blank">terms of use</a> for default API!.'];
