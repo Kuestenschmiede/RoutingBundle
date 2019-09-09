f02981a204aea4d65e0505adbd021af4f8e71fd3
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2019-09-09 18:04:09 +0200

    remove features on loading new ones

--- Resources/public/js/components/c4g-router-view.jsx
+++ Resources/public/js/components/c4g-router-view.jsx
@@ -869,6 +869,9 @@ export class RouterView extends Component {
       overCoord;
 
     scope = this;
+    if (this.state.featureSource) {
+      this.state.featureSource.clear();
+    }
     // this.areaSource.clear();
     this.mapSelectInteraction.getFeatures().clear();
     if (!fromPoint) {
@@ -1014,10 +1017,9 @@ export class RouterView extends Component {
       return [];
     }
     const mapData = this.mapData;
-    let layerId = mode === "router" ? jQuery(this.routerLayersSelect).val() : jQuery(this.areaLayersSelect).val();
-    layerId = 4;
+    let layerId = this.state.mode === "route" ? this.state.layerRoute : this.state.layerArea;
     const layer = self.props.mapController.proxy.layerController.arrLayers[layerId];
-    let activeLayer = mode === "router" ? 4: 4;//self.activeLayerValue : self.activeLayerValueArea; //richtig anpassen, wenn vorhanden aus GUI
+    let activeLayer = this.state.mode === "route" ? "Tanken": "Tanken";//self.activeLayerValue : self.activeLayerValueArea; //richtig anpassen, wenn vorhanden aus GUI
     const unstyledFeatures = [];
     const contentFeatures = [];
     let missingStyles = [];
