828501cdf98ba4ee1ddfafeda1bb91b9376f8813
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-12-19 14:56:25 +0100

    Remove detail class when result container is closed

--- Resources/public/js/components/c4g-router-result-container.jsx
+++ Resources/public/js/components/c4g-router-result-container.jsx
@@ -123,7 +123,8 @@ export class RouterResultContainer extends Component {
         );
     }
     return (
-      <div className={this.props.className + (this.props.open ? " c4g-open" : " c4g-close") + (this.props.detailOpen ? " c4g-details-open" : "")}>
+      <div className={this.props.className + (this.props.open ? " c4g-open" : " c4g-close")
+      + (this.props.detailOpen && this.props.open ? " c4g-details-open" : "")}>
           {routerHeader}
           {resultSwitcher}
           {result}
