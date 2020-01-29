c2afee9f580399f7843aade12253ba511e4d41ac
coastforge_mei <matthias.eilers@kuestenschmiede.de>
2020-01-29 15:23:38 +0100

    removed old profile locstyle loading

--- Classes/Callbacks/TlC4gRoutingConfiguration.php
+++ Classes/Callbacks/TlC4gRoutingConfiguration.php
@@ -102,16 +102,12 @@ class TlC4gRoutingConfiguration
      */
     public function getLocStyles(DataContainer $dc)
     {
-        $profile = $this->db->prepare('SELECT locstyles FROM tl_c4g_map_profiles WHERE id=?')->execute($dc->activeRecord->profile);
-        $ids = deserialize($profile->locstyles, true);
-        if (count($ids) > 0) {
-            $locStyles = $this->db->prepare('SELECT id,name FROM tl_c4g_map_locstyles WHERE id IN (' . implode(',', $ids) . ') ORDER BY name')->execute();
-        } else {
-            $locStyles = $this->db->prepare('SELECT id,name FROM tl_c4g_map_locstyles ORDER BY name')->execute();
-        }
+        $locStyles = $this->db->prepare('SELECT id,name FROM tl_c4g_map_locstyles ORDER BY name')->execute();
         $return[0] = '-';
-        while ($locStyles->next()) {
-            $return[$locStyles->id] = $locStyles->name;
+        if ($locStyles) {
+            while ($locStyles->next()) {
+                $return[$locStyles->id] = $locStyles->name;
+            }
         }
 
         return $return;
